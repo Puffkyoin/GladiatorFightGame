3c2b8df6c6edc84bcd2a4c42ffd350b01249e966
Puffkyoin <103963648+Puffkyoin@users.noreply.github.com>
2022-05-11 05:30:03 +0300

    fix formula and new variables

--- Gladiator Fight/Program.cs
+++ Gladiator Fight/Program.cs
@@ -8,23 +8,30 @@ namespace Gladiator_Fight
         public static void Main(string[] args)
         {
             Random rand = new Random();
-            float health1 = rand.Next(90, 101); 
-            int damage1 = rand.Next(5, 21);
-            int armor1= rand.Next(25, 66) ;
-            float health2 = rand.Next(80, 151);
-            int damage2 = rand.Next(25, 50);
-            int armor2= rand.Next(15, 35);
-            int hitNumber = 0 ;
+            int minHealth = 100, maxHealth = 201;
+            // базовые минимальные и максимальные значения для armor и damage
+            int minValue = 25, maxValue = 50;
+            // константа преимущества
+            int advantage = 25;
+            int health1 = rand.Next(minHealth, maxHealth);
+            int damage1 = rand.Next(minValue + advantage, maxValue + advantage);
+            int armor1 = rand.Next(minValue, maxValue);
+            int health2 = rand.Next(minHealth, maxHealth);
+            int damage2 = rand.Next(minValue, maxValue);
+            int armor2 = rand.Next(minValue + advantage, maxValue + advantage);
+            int hitNumber = 0;
             int divider = 100;
 
             Console.WriteLine($"Гладиатор1: \nЗдоровье - {health1}\nЗашита - {armor1}\nАтака - {damage1}\n");
             Console.WriteLine($"Гладиатор2: \nЗдоровье - {health2}\nЗашита - {armor2}\nАтака - {damage2}\n");
+            Console.WriteLine("Битва начинается!");
             Console.ReadKey();
 
             while (health1 > 0 && health2 > 0)
             {
-                health1 -= Convert.ToSingle(rand.Next(0, damage2)) / divider * armor1;
-                health2 -= Convert.ToSingle(rand.Next(0, damage1)) / divider * armor2;
+                // формула health -= damage * (1 - armor / 100)
+                health1 -= Convert.ToInt32(rand.Next(0, damage2) * (1 - Convert.ToSingle(armor1 / divider)));
+                health2 -= Convert.ToInt32(rand.Next(0, damage1) * (1 - Convert.ToSingle(armor2 / divider)));
                 Console.WriteLine($"Стычка №{++hitNumber}");
                 Console.WriteLine($"Гладиатор1 - {health1} здоровья.");
                 Console.WriteLine($"Гладиатор2 - {health2} здоровья.\n");
@@ -38,11 +45,10 @@ namespace Gladiator_Fight
             {
                 Console.WriteLine("Первый гладиатор пал!");
             }
-            else if(health2 <= 0)
+            else if (health2 <= 0)
             {
                 Console.WriteLine("Второй гладиатор повержен!");
             }
-            
         }
     }
 }
